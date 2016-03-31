edb04494b47c0dc6bad0edcee8a23e6efb0aca63
sebastiaan <sebastiaan.heunis@spandigital.com>
2016-03-31 17:50:44 +0200

    Added some basic styling

--- js/components/Notifications.react.js
+++ js/components/Notifications.react.js
@@ -19,14 +19,14 @@ export default class Notifications extends Component {
     render() {
         const { notifications, notifications_errors, on_click } = this.props;
         return (
-            <section className="text-section">
-                <h2>Notifications</h2>
+            <section className="text-section notifications-widget">
+                <div className="header-row"><h4 className="pull-left">Notifications</h4></div>
                 <table className="notifications-table">
                     <tr><th>Message</th><th>Link</th><th>Date Logged</th><th>Priority</th><th>Dismiss</th></tr>
                     {notifications.map(function(notification) {
                         return <tr key={notification.id}><td>{notification.message}</td><td>{notification.link}</td><td>{notification.date_logged}</td>
                             <td>{notification.priority}</td>
-                            <td><input type="button" onClick={on_click.bind(this, notification)} value="Dismiss"/></td></tr>;
+                            <td><input type="button" className="btn btn-default" onClick={on_click.bind(this, notification)} value="Dismiss"/></td></tr>;
                     })}
                 </table>
             </section>
